3ffbce3b1af431f380207c8b2429a0141dca5b6f
pholmq <pholmq@gmail.com>
2016-11-09 14:56:55 +0000

    en liten ändring

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
 import { Parties } from '../../../both/collections/parties.collection';
 
 import template from './app.component.html';
