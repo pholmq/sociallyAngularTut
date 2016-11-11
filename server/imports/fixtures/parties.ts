8209edbad4d94d24bd483cc0b24cf018c473b044
pholmq <pholmq@gmail.com>
2016-11-11 16:53:48 +0000

    chapt 3

--- server/imports/fixtures/parties.ts
+++ server/imports/fixtures/parties.ts
@@ -1,9 +1,24 @@
 import { Parties } from '../../../both/collections/parties.collection';
+import { Party } from '../../../both/models/party.model';
 
 export function loadParties() {
   if (Parties.find().cursor.count() === 0) {
-    const parties = [(
-      name: 'Dubstep free zone'
-    )]
+    const parties = [{
+      name: 'Dubstep free zone',
+      description: 'No more Dubstep',
+      location: 'Palo Alto'
+    }, {
+      name: 'Dubstep att the way',
+      description: 'Get in on',
+      location: 'Gothenburg'
+    }, {
+      name: 'Loungin',
+      description: 'Chill',
+      location: 'San Fran'
+      
+    }];
+
+    parties.forEach((party: Party) => Parties.insert(party));
+
   }
 }
